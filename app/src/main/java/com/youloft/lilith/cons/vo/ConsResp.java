80f39d26de6723026a6096b216155e6c0ac6f7e3
coder <ckjavacoder@icloud.com>
2017-06-26 18:56:12 +0800

    响应体实体

---
+++ app/src/main/java/com/youloft/lilith/cons/vo/ConsResp.java
@@ -0,0 +1,14 @@
+package com.youloft.lilith.cons.vo;
+
+import com.youloft.lilith.common.net.AbsResponse;
+
+/**
+ * 星座接口响应类
+ * <p>
+ * Created by coder on 2017/6/26.
+ */
+
+public class ConsResp extends AbsResponse {
+
+
+}
