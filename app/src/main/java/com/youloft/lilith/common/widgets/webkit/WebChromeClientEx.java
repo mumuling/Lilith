25abd90c48d8b1457a25680e0a17e8a3f068bcf9
coder <ckjavacoder@icloud.com>
2017-06-27 10:29:39 +0800

    响应体实体

---
+++ app/src/main/java/com/youloft/lilith/common/widgets/webkit/WebChromeClientEx.java
@@ -0,0 +1,14 @@
+package com.youloft.lilith.common.widgets.webkit;
+
+import android.webkit.WebChromeClient;
+
+/**
+ * WebChromeClientEx增强类
+ * <p>
+ * Created by coder on 2017/6/27.
+ */
+
+public class WebChromeClientEx extends WebChromeClient {
+
+
+}
