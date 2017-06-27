f6189ccf2e025839a54943ecbd09c142909b62c6
coder <ckjavacoder@icloud.com>
2017-06-27 11:25:02 +0800

    UrlProtocol实例

--- app/src/main/java/com/youloft/lilith/common/widgets/webkit/URLProtocolHandler.java
+++ app/src/main/java/com/youloft/lilith/common/widgets/webkit/URLProtocolHandler.java
@@ -1,6 +1,8 @@
 package com.youloft.lilith.common.widgets.webkit;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.text.TextUtils;
 import android.webkit.WebView;
 
@@ -20,7 +22,8 @@ public class URLProtocolHandler {
      */
     public boolean handleUrl(WebView view, String url) {
         if (url.toLowerCase().startsWith("protocol")) {
-            return handleProtocolUrl(view, url);
+            handleProtocolUrl(view, url);
+            return true;
         }
         if (url.toLowerCase().startsWith("mailto")) {
             return handleMailToProtocol(view, url);
@@ -60,7 +63,101 @@ public class URLProtocolHandler {
      * @param url
      * @return
      */
-    protected boolean handleProtocolUrl(WebView view, String url) {
-        return false;
+    private void handleProtocolUrl(WebView view, String url) {
+        try {
+            String actionUrl = url.substring("protocol://".length());
+            int dotIndex = actionUrl.indexOf("#");
+            if (dotIndex < 0) {
+                dotIndex = actionUrl.indexOf(":");
+                if (dotIndex < 0) {
+                    dotIndex = actionUrl.length();
+                }
+            }
+            String action = actionUrl.substring(0, dotIndex);
+            String paramStr = actionUrl.substring(dotIndex);
+            handleProtocolImpl(view, url, action.toLowerCase(), paramStr);
+        } catch (Exception e) {
+        }
     }
+
+    /**
+     * 处理Protocol协议
+     *
+     * @param view
+     * @param url
+     * @param action
+     * @param paramStr
+     * @return
+     */
+    private void handleProtocolImpl(WebView view, String url, String action, String paramStr) {
+        if (TextUtils.isEmpty(action)) {
+            return;
+        }
+        switch (action) {
+            case "rate":
+                handleRateApp(view.getContext());
+            case "back":
+                handleWebBack(view);
+            case "exit":
+                handleClose();
+            case "share":
+                handleShare(view, url, action, paramStr);
+            case "getfilecode":
+                handleFileChoose(view, url, action, paramStr);
+        }
+    }
+
+    /**
+     * 处理文件选择
+     *
+     * @param view
+     * @param url
+     * @param action
+     * @param paramStr
+     */
+    protected void handleFileChoose(WebView view, String url, String action, String paramStr) {
+
+    }
+
+    /**
+     * 处理分享支持
+     *
+     * @param view
+     * @param url
+     * @param action
+     * @param paramStr
+     */
+    protected void handleShare(WebView view, String url, String action, String paramStr) {
+
+    }
+
+    /**
+     * 处理关闭
+     */
+    protected void handleClose() {
+    }
+
+    /**
+     * 处理网页回退
+     *
+     * @param view
+     */
+    protected void handleWebBack(WebView view) {
+        if (view.canGoBack()) {
+            view.goBack();
+        }
+    }
+
+    /**
+     * 处理应用评分
+     *
+     * @param context
+     */
+    private void handleRateApp(Context context) {
+        Uri uri = Uri.parse("market://details?id=" + context.getPackageName());
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(Intent.createChooser(intent, "选择评论的市场"));
+    }
+
 }
