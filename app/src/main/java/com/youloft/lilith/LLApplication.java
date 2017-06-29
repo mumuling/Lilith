6877a39afb7187328de7d0350b386a154e4ad0dd
coder <ckjavacoder@icloud.com>
2017-06-29 10:57:26 +0800

    处理冲突，删除Util

--- app/src/main/java/com/youloft/lilith/LLApplication.java
+++ app/src/main/java/com/youloft/lilith/LLApplication.java
@@ -5,9 +5,6 @@ import android.content.Context;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.youloft.lilith.common.cache.CacheStore;
-import com.youloft.lilith.common.net.AppEnv;
-//import com.youloft.lilith.common.net.NetUtil;
-//import com.youloft.lilith.common.net.OnlineConfigAgent;
 import com.youloft.lilith.common.utils.Utils;
 import com.youloft.lilith.push.PushMessageHandler;
 import com.youloft.lilith.push.PushNotificationClickHandler;
@@ -20,6 +17,41 @@ import com.youloft.push.PushApp;
  */
 public class LLApplication extends Application {
 
+
+    private static LLApplication sInstance = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+        AppConfig.init(this);
+        Utils.init(this);
+        initARouter();
+    }
+
+    /**
+     * 初始化ARouter
+     */
+    private void initARouter() {
+        if (AppConfig.DebugMode) {
+            ARouter.openLog();     // 打印日志
+            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        }
+        ARouter.init(this);
+    }
+
+    public static LLApplication getInstance() {
+        return sInstance;
+    }
+
+    /**
+     * 获取APi类别缓存
+     *
+     * @return
+     */
+    public static CacheStore getApiCache() {
+        return CacheStore.getStore("api");
+    }
 
     /**
      * 初始化Push
@@ -37,42 +69,4 @@ public class LLApplication extends Application {
                 .setMessageHandler(new PushMessageHandler())
                 .setNotificationClickHandler(new PushNotificationClickHandler());
     }
-
-    private static LLApplication mContext = null;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        mContext = this;
-        AppConfig.init(this);
-        Utils.init(this);
-        initARouter();
-        setChannelBundle();
-        NetUtil.getInstance().initPublicParam();
-    }
-
-    /**
-     * 初始化ARouter
-     */
-    private void initARouter() {
-        if (Util.isApkInDebug(mContext)) {
-            ARouter.openLog();     // 打印日志
-            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
-        }
-        ARouter.init(this);
-    }
-
-    public static Context getContext() {
-        return mContext;
-    }
-
-    /**
-     * 获取APi类别缓存
-     *
-     * @return
-     */
-    public static CacheStore getApiCache() {
-        return CacheStore.getStore("api");
-    }
-
 }
