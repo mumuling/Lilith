19f02ac08b29ff101d1d91dc98adcca4c5ab4361
guoyonghong <yuandian1990>
2017-06-29 14:26:31 +0800

    登录布局,暂时搁浅

---
+++ app/src/main/java/com/youloft/lilith/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.youloft.lilith.login;
+
+import android.os.Bundle;
+
+import com.youloft.lilith.R;
+import com.youloft.lilith.common.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
