f7d2cba592bf2311e4701fea22659aee12f2f133
slj <765559466@qq.com>
2017-06-29 15:33:41 +0800

    Glide处理圆形图片

--- app/src/main/java/com/youloft/lilith/topic/TopicDetailActivity.java
+++ app/src/main/java/com/youloft/lilith/topic/TopicDetailActivity.java
@@ -2,22 +2,35 @@ package com.youloft.lilith.topic;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.youloft.lilith.R;
+import com.youloft.lilith.common.GlideApp;
+import com.youloft.lilith.common.GlideRequest;
+import com.youloft.lilith.common.GlideRequests;
 import com.youloft.lilith.common.base.BaseActivity;
+import com.youloft.lilith.ui.GlideCircleTransform;
 import com.youloft.lilith.ui.view.BaseToolBar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
+ * 星座话题的详情页
+ * version
  *
+ * @author slj
+ * @time 2017/6/29 14:10
+ * @class TopicDetailActivity
  */
 
 public class TopicDetailActivity extends BaseActivity {
 
     @BindView(R.id.tool_bar)
     BaseToolBar toolBar;
+    @BindView(R.id.circle_image)
+    ImageView circleImage;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,6 +42,10 @@ public class TopicDetailActivity extends BaseActivity {
 
     private void initView() {
         toolBar.setTitle("星座话题");
+        GlideApp.with(this).asBitmap()
+                .load(R.mipmap.er)
+                .transform(new GlideCircleTransform(this))
+                .into(circleImage);
         toolBar.setOnToolBarItemClickListener(new BaseToolBar.OnToolBarItemClickListener() {
             @Override
             public void OnBackBtnClick() {
--- app/src/main/java/com/youloft/lilith/ui/fragment/CCFragment.java
+++ app/src/main/java/com/youloft/lilith/ui/fragment/CCFragment.java
@@ -7,8 +7,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.youloft.lilith.R;
+import com.youloft.lilith.common.GlideApp;
 import com.youloft.lilith.common.base.BaseFragment;
-import com.youloft.lilith.common.base.GlideApp;
 import com.youloft.lilith.common.net.IRequestResult;
 import com.youloft.lilith.common.net.OkHttpUtils;
 import com.youloft.lilith.common.utils.LogUtil;
@@ -35,30 +35,6 @@ public class CCFragment extends BaseFragment {
                 .load("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498627427494&di=3b1ca86fd98e8044541f68a8e5243902&imgtype=0&src=http%3A%2F%2Ftupian.enterdesk.com%2F2015%2Fxu%2F04%2F20%2F12%2Fzweihua11.jpg")
                 .into(img);
 
-        String url = "http://apis.baidu.com/showapi_open_bus/pic/pic_search";
-        String apikey = "99d5fcb6ca3f46b33431daa2b02dac04";
-        String type = "4002";
-        String page = "1";
-        HashMap<String, String> param = new HashMap<>();
-        HashMap<String, String> hparam = new HashMap<>();
-        hparam.put("apikey", apikey);
-        param.put("type", type);
-        param.put("page", page);
-        OkHttpUtils.getInstance().requestEnqueue(url, hparam, param, false, String.class, new IRequestResult<String>() {
-            @Override
-            public void onSuccessful(String result) {
-                tv.setText(result);
-            }
 
-            @Override
-            public void onFailure(String errorMsg) {
-                tv.setText(errorMsg);
-            }
-
-            @Override
-            public void onCompleted() {
-                LogUtil.d("aaa", "wanc");
-            }
-        });
     }
 }
