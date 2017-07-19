3add47db7b2b65fed2e7925baced12fb745f2a9a
zc <zchaoysu@163.com>
2017-07-19 12:58:13 +0800

    隐私条款

--- app/src/main/java/com/youloft/lilith/common/widgets/dialog/PrivacyDialog.java
+++ app/src/main/java/com/youloft/lilith/common/widgets/dialog/PrivacyDialog.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.StyleRes;
 import android.text.Html;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.TextView;
 
